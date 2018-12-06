ef734e53448630cef13b1a7aeb39bb242ed5f569
Jonas Frey <uni@jonasfrey.de>
2018-12-06 15:56:23 +0100

    Extracted some code into a new class BackupController.swift Map name gets now chosen from the name of the instance (the instance name should contain the map name for this to work) The extracted backup is now temporarily stored in /tmp instead of the backup directory

---
+++ ARK-Backup-Restorer/BackupController.swift
@@ -0,0 +1,179 @@
+//
+//  BackupController.swift
+//  ARK-Backup-Restorer
+//
+//  Created by Jonas Frey on 04.12.18.
+//  Copyright © 2018 Jonas Frey. All rights reserved.
+//
+
+/*
+ let home = FileManager.default.homeDirectoryForCurrentUser.path
+ let backupDayDirectory = "\(home)/ARK-Backups/\(backupDay)"
+ var unzippedFolder = "\(backupDayDirectory)/\(backupDay)_\(time)"
+ var backupZipFilePath = "\(backupDayDirectory)/the_island.\(backupDay)_\(time)"
+ var destination = "\(home)/\(instance)/ShooterGame/Saved/SavedArks"
+ */
+
+import Foundation
+
+class BackupController {
+    
+    let instance: String
+    var backupTime: String
+    let backupDay: String
+    let basePath = FileManager.default.homeDirectoryForCurrentUser.path + "/Backups/"
+    
+    init(instance: String, time: String, isYesterday: Bool) {
+        let formatter = DateFormatter()
+        
+        self.instance = instance
+        self.backupTime = time
+        // If no time provided by argument, use now
+        if time.isEmpty {
+            formatter.dateFormat = "HH.mm"
+            self.backupTime = formatter.string(from: Date())
+            // round the time down, so we get a time modulo 10 minutes
+            self.backupTime.removeLast()
+            self.backupTime += "0"
+        }
+        
+        // Set the backup day
+        formatter.dateFormat = "yyyy-MM-dd"
+        var date = Date()
+        if isYesterday {
+            // Set the date to yesterday
+            date.addTimeInterval(-1 * 60 * 60 * 24)
+        }
+        self.backupDay = formatter.string(from: date)
+    }
+    
+    func restoreBackup() {
+        // the backup zip file (full path)
+        let zip = getBackupFile()
+        if zip == nil {
+            print("Error getting backup file!")
+            exit(EXIT_FAILURE)
+        }
+        
+        // the folder with the contents of the extracted zip
+        let sourceFolder = "/tmp/" + String(zip!.components(separatedBy: "/").last!.dropLast(8)) + "/"
+        // the ark savegame directory (/Saved/SavedArks/)
+        let destination = basePath + instance + "/ShooterGame/Saved/SavedArks/"
+        
+        // Check, if the savegame directory exists
+        if !FileManager.default.fileExists(atPath: destination) {
+            print("Invalid instance")
+            exit(EXIT_FAILURE)
+        }
+        
+        // Check, if the backup file for the given time exists
+        if !FileManager.default.fileExists(atPath: zip!) {
+            print("Backup file " + zip! + " does not exist!")
+        }
+        
+        print("Backup found: " + zip!)
+        print("Destination: " + destination)
+        print("Extracting Backup...")
+        
+        // Decompressing archive into /tmp
+        let output = shell(launchPath: "/bin/tar", dir: "/tmp", arguments: "-xf", zip!)
+        if !output.isEmpty {
+            print("tar: \(output)")
+        }
+        
+        print("Restoring backup...")
+        
+        // Restore the contents of the extracted folder in /tmp into the save game directory
+        restore(sourceFolder: sourceFolder, destinationFolder: destination)
+        
+        print("Backup successfully restored.")
+
+    }
+    
+    private func getBackupFile() -> String? {
+        let folder = basePath + backupDay + "/"
+        // Sets the file name with the time (rounded down to 10) without the seconds
+        let filename = getMap() + "." + backupDay + "_" + backupTime
+        // Get exact second of backup (find matching file name)
+        for i in 0...9 {
+            if FileManager.default.fileExists(atPath: "\(folder + filename).0\(i).tar.bz2") {
+                // Found a valid backup file
+                return folder + filename + ".0\(i).tar.bz2"
+            }
+        }
+        return nil
+    }
+    
+    private func getMap() -> String {
+        return BackupController.mapForInstance(instance)
+    }
+    
+    class func mapForInstance(_ instance: String) -> String {
+        if instance.lowercased().contains("extinction") {
+            return "extinction"
+        } else if instance.lowercased().contains("scorched") {
+            return "scorched_earth"
+        } else if instance.lowercased().contains("center") {
+            return "the_center"
+        } else if instance.lowercased().contains("aberration") {
+            return "aberration"
+        } else if instance.lowercased().contains("island") {
+            return "the_island"
+        }
+        return "the_island"
+    }
+    
+    private func restore(sourceFolder: String, destinationFolder: String) {
+        do {
+            let fileManager = FileManager.default
+            // Remove the savegame backup that was created last time
+            if fileManager.fileExists(atPath: "\(destinationFolder).old") {
+                print("  Removing old data folder")
+                try fileManager.removeItem(atPath: "\(destinationFolder).old")
+            }
+            
+            print("  Backing up data folder")
+            try fileManager.moveItem(atPath: destinationFolder, toPath: "\(destinationFolder).old")
+            print("  Creating new data folder")
+            try fileManager.createDirectory(atPath: destinationFolder, withIntermediateDirectories: false)
+            
+            // Restore the backup
+            print("  Getting contents of \(sourceFolder)")
+            let contents = try fileManager.contentsOfDirectory(atPath: sourceFolder)
+            print("  Moving files")
+            for file in contents {
+                print("    \(file)")
+                try fileManager.moveItem(atPath: "\(sourceFolder)\(file)", toPath: "\(destinationFolder)\(file)")
+            }
+            
+            print("Removing extracted backup...")
+            // Remove the folder
+            try fileManager.removeItem(atPath: sourceFolder)
+        } catch {
+            print("Error while restoring backup!")
+            print(error)
+            exit(EXIT_FAILURE)
+        }
+    }
+    
+    /// Run a shell command
+    @discardableResult
+    private func shell(launchPath: String, dir: String, arguments: String...) -> String {
+        let task = Process()
+        task.currentDirectoryPath = dir
+        task.launchPath = launchPath
+        task.arguments = arguments
+        
+        let pipe = Pipe()
+        task.standardOutput = pipe
+        task.launch()
+        
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        let output: String = String(data: data, encoding: String.Encoding.utf8)!
+        
+        return output
+    }
+    
+    
+    
+}
--- ARK-Backup-Restorer/main.swift
+++ ARK-Backup-Restorer/main.swift
@@ -8,36 +8,14 @@
 
 import Foundation
 
-let usage = "Usage: \(CommandLine.arguments.first!) [-d] <instance> <hh:mm>"
-
-/// Run a shell command
-@discardableResult
-func shell(launchPath: String, dir: String, arguments: String...) -> String {
-    let task = Process()
-    task.currentDirectoryPath = dir
-    task.launchPath = launchPath
-    task.arguments = arguments
-    
-    let pipe = Pipe()
-    task.standardOutput = pipe
-    task.launch()
-    
-    let data = pipe.fileHandleForReading.readDataToEndOfFile()
-    let output: String = String(data: data, encoding: String.Encoding.utf8)!
-    
-    return output
-}
-
-// MARK: Preparation
-let formatter = DateFormatter()
-var backupTime = ""
+public let usage = "Usage: \(CommandLine.arguments.first!) [-d] <instance> <hh:mm>"
 
 // MARK: Argument Parsing
 var useYesterday = false
 var time: String
 var instance: String
 
-// Remove the first arguments (because its the file name)
+// Remove the first argument (the file name)
 var arguments = Array(CommandLine.arguments.dropFirst())
 
 // -d
@@ -67,7 +45,7 @@ if arg.range(of: timeRegex, options: .regularExpression, range: nil, locale: nil
     exit(EXIT_FAILURE)
 }
 // Get time
-backupTime = arg.replacingOccurrences(of: ":", with: ".")
+time = arg.replacingOccurrences(of: ":", with: ".")
 arguments.removeFirst()
 
 if !arguments.isEmpty {
@@ -76,97 +54,8 @@ if !arguments.isEmpty {
 }
 
 // MARK: Execution
-
-formatter.dateFormat = "yyyy-MM-dd"
-var date = Date()
-if useYesterday {
-    // Set the date to yesterday
-    date.addTimeInterval(-1 * 60 * 60 * 24)
-}
-let backupDay = formatter.string(from: date)
-
-// If no time provided by argument
-if backupTime.isEmpty {
-    formatter.dateFormat = "HH.mm"
-    backupTime = formatter.string(from: Date())
-    // round the time down, so we get a time modulo 10 minutes
-    backupTime.removeLast()
-    backupTime += "0"
-}
-
-// MARK: Define directories
-let home = FileManager.default.homeDirectoryForCurrentUser.path
-let backupDayDirectory = "\(home)/ARK-Backups/\(backupDay)"
-var unzippedFolder = "\(backupDayDirectory)/\(backupDay)_\(backupTime)"
-var backupZipFilePath = "\(backupDayDirectory)/the_island.\(backupDay)_\(backupTime)"
-var destination = "\(home)/\(instance)/ShooterGame/Saved/SavedArks"
-//var destination = "\(home)/test"
-//print("[WARNING] Debug Destination set to \(destination)")
-
-// Get exact second of backup (find matching file name)
-for i in 0...9 {
-    if FileManager.default.fileExists(atPath: "\(backupZipFilePath).0\(i).tar.bz2") {
-        backupZipFilePath += ".0\(i).tar.bz2"
-        unzippedFolder += ".0\(i)"
-        break
-    }
-}
-
-// Exact Zipfile name
-let backupZipFileName = backupZipFilePath.components(separatedBy: "/").last!.dropLast(8)
-
-print("Destination: \(destination)")
-if !FileManager.default.fileExists(atPath: destination) {
-    print("Invalid instance")
-    exit(EXIT_FAILURE)
-}
-
-//print("Backup directory: \(backupDayDirectory)")
-print("Accessing backup file \(backupZipFileName).tar.bz2")
-
-if !backupZipFilePath.hasSuffix(".tar.bz2") {
-    // File did not exist
-    print("The backup for the given time does not exist")
-    exit(EXIT_FAILURE)
-}
-
-// Decompressing archive
-let output = shell(launchPath: "/bin/tar", dir: "\(backupDayDirectory)", arguments: "-xf", backupZipFilePath)
-if !output.isEmpty {
-    print("tar: \(output)")
-}
-
-print("Restoring backup...")
-
-do {
-    let fileManager = FileManager.default
-    // Backup the current savegame
-    if fileManager.fileExists(atPath: "\(destination).old") {
-        print("  Removing old data folder")
-        try fileManager.removeItem(atPath: "\(destination).old")
-    }
-    print("  Backing up data folder")
-    try fileManager.moveItem(atPath: destination, toPath: "\(destination).old")
-    print("  Creating new data folder")
-    try fileManager.createDirectory(atPath: destination, withIntermediateDirectories: false)
-    // Restore the backup
-    print("  Getting contents of \(unzippedFolder)")
-    let contents = try fileManager.contentsOfDirectory(atPath: unzippedFolder)
-    print("  Moving files")
-    for file in contents {
-        // print("    \(file)")
-        try fileManager.moveItem(atPath: "\(unzippedFolder)/\(file)", toPath: "\(destination)/\(file)")
-    }
-    print("Removing decompressed backup")
-    // Remove the folder
-    try fileManager.removeItem(atPath: unzippedFolder)
-} catch {
-    print("Error while restoring backup!")
-    print(error)
-    exit(EXIT_FAILURE)
-}
-
-print("Backup successfully restored.")
+let controller = BackupController(instance: instance, time: time, isYesterday: useYesterday)
+controller.restoreBackup()
 
 exit(EXIT_SUCCESS)
 
